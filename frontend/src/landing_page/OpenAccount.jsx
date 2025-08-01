b396ba14f38d1b588b1608118742d78a2b4dd065
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-08-01 08:59:32 +0530

    openAccount done

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,7 +1,25 @@
 function OpenAccount() {
-    return ( <div>
-        <h1>OpenAccount</h1>
-    </div> );
+    return (<div className="container p-5 mb-5 mt-5">
+        <div className="row text-center">
+          <img
+            src="Images/homeHero.png"
+            className="img-fluid mb-5"
+            alt="Hero img"
+            srcset=""
+          />
+          <h1 className="mt-5 mb-4">Open a Zerodha account</h1>
+  
+          <p className="text-muted mb-4">
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+          </p>
+          <button
+            style={{ width: "20%", margin: "auto" }}
+            className="p-2 btn btn-primary fs-5"
+          >
+            SignUp for free
+          </button>
+        </div>
+      </div> );
 }
 
 export default OpenAccount;
