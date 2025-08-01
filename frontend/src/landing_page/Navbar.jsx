9c3a6508f23188d71f3020d76b2c872f762c1d10
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-08-01 08:51:56 +0530

    Navbar Done

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,7 +1,38 @@
 function Navbar() {
-    return ( <div>
-        <h1>Navbar</h1>
-    </div> );
+    return ( 
+       <nav class="navbar navbar-expand-lg border-bottom " style={{backgroundColor:"FFF",height:"75px"}}>
+  <div class="container">
+    <a class="navbar-brand" href="#"><img style={{width:"30%"}} src="Images/logo.svg" alt="" srcset="" /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     
+      <form class="d-flex" role="search">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Sign up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Support</a>
+        </li>
+       
+       
+      </ul>
+      </form>
+    </div>
+  </div>
+</nav>
+);
 }
 
 export default Navbar;
