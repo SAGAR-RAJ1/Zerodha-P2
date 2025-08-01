59e0ae697a2f698ade3791b46745cdb0ce979af1
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-08-01 06:55:00 +0530

    done Hero.jsx in home

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,9 +1,28 @@
 function Hero() {
-    return ( 
-        <div>
-       <h1>Hero</h1>
-        </div>
-     );
+  return (
+    <div className="container p-5">
+      <div className="row text-center">
+        <img
+          src="Images/homeHero.png"
+          className="img-fluid mb-5"
+          alt="Hero img"
+          srcset=""
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more. Sign up for free
+        </p>
+        <button
+          style={{ width: "20%", margin: "auto" }}
+          className="p-1 btn btn-primary fs-5"
+        >
+          SignUp Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
