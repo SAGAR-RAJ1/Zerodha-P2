1299aa9362796db0654f8948ac802f85de312ad8
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-08-06 00:37:06 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,19 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const cors = require("cors")
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
+
 const URL = process.env.MONGO_URL;
+
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+
+
+
+
 main()
   .then(() => {
     console.log("connection Successful");
@@ -17,12 +26,37 @@ async function main() {
   await mongoose.connect(URL);
 }
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views",path.join(__dirname, "views"));
 app.get('/', function(req, res) {
     res.send("welcome");
+});
+
+app.get('/allHoldings',async function(req, res) {
+
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings)
+    // res.send("welcome");
+});
+app.get('/allPositions',async function(req, res) {
+
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions)
+    // res.send("welcome");
+});
+app.post('/newOrder',async function(req, res) {
+
+  let newOrder = new OrdersModel({  
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode});
+  newOrder.save();
+  res.send("order Saved")
+
 });
 app.listen(PORT);
